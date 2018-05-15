3d0de7a498d97faedd4a01fa88444a56167d2a75
muskan6769 <iit2017046@iiita.ac.in>
2018-05-15 09:40:51 +0530

    graph ploting

--- climate/climate/views.py
+++ climate/climate/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from django.http import HttpResponseRedirect
 
+
+time=""
+name=""
+graph1=""
+
 class HomePage(View):
     def get(self, request):
         return render(request, 'index.html')
         
 class user1(View):
     def post(self,request):
+        global name
         name=request.POST.get("topic")
         print(name)
         #return render(request,'index.html',locals())
@@ -17,20 +23,26 @@ class user1(View):
 
 class zone(View):
     def post(self,request):
+        global time
         time=request.POST.get("zone")
         print(time)
         #return render(request,'index.html')
         return HttpResponseRedirect("/")
 
+
+
 class types(View):
     def post(self,request):
         t=request.POST.get("graph")
-        print(t)
+        myVars={'time':time,'name':name,'graph1':t}
+        exec(open('plots.py').read(),myVars)
+       # k=5
+        #exec(open('plots.py').read())
         # import subprocess
         # subprocess.Popen("plots.py", shell=True)
         
-        import plots
-        return render(request,'1.html')
-        #return HttpResponseRedirect("/")
+       # import plots
+        #return render(request,'1.html')
+        return HttpResponseRedirect("/")
         #subprocess.run(['python','plots.py', name,time,t], shell=True)
 
--- climate/plots.py
+++ climate/plots.py
@@ -1,7 +1,27 @@
+from __main__ import *
+import random
 import numpy as np
 import matplotlib.pyplot as plt
-print(21)
-x=[2,3,4,5]
-y=[4,9,16,25]
-plt.plot(x,y)
+
+if time=="1900-2000":
+        data=np.genfromtxt('1.csv',delimiter=',',dtype=None,skip_header=1,names=('year','Max_Temp','Min_Temp','Mean_Temp'))
+        k=np.random.rand(100)
+elif time=="2001-2016":
+        data=np.genfromtxt('2.csv',delimiter=',',dtype=None,skip_header=1,names=('year','Max_Temp','Min_Temp','Mean_Temp'))
+        k=np.random.rand(16)
+
+if graph1=="line-graph":
+    plt.plot(data['year'],data[name])
+
+elif graph1=="scatter-plot":
+    plt.scatter(data['year'],data[name],alpha=0.8,c=k)
+
+
+elif graph1=="bar-graph":
+    plt.bar(data['year'],data[name])
+
+plt.title(name+' in India')
+plt.ylabel("Temp in degree celcius")
+plt.xlabel("year")
+#plt.clf()
 plt.show()
